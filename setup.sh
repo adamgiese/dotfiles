492a16fd0a0d55b6598bd40a28a72033ed7bf669
Adam Giese <adam.giese@underarmour.com>
2020-04-19 22:00:26 -0500

    Update name of repo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 BACKUP_DIR="$HOME/config_backup_`date +%Y%m%d_%H%M%S`"
 mkdir "$BACKUP_DIR"
 
-for cfile in $(find "$HOME/.dotfiles/dotfiles" -mindepth 1 -maxdepth 1); do
+for cfile in $(find "$HOME/dotfiles/dotfiles" -mindepth 1 -maxdepth 1); do
     existing="$HOME/${cfile##*/}"
     if [[ -f "$existing" || -L "$existing" ]]; then
         mv "$existing" "$BACKUP_DIR";
